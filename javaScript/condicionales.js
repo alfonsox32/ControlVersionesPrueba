507e62720006a364b37836eeac75569ae9a6375d
Alfonso <alfonsovargas2266@gmail.com>
2025-08-04 13:59:25 -0600

    Toda la artilleria web

---
+++ javaScript/condicionales.js
@@ -0,0 +1,28 @@
+'use strict'
+//condicional if
+
+var edad2 =95;
+var nombre = 'alfonso';
+var numero45 = 8727716518;
+
+if (edad2 == 18 || edad2 ==95) {
+    console.log('Felicidades eres mayor de edad tu edad es: '+edad2);
+}else if (edad2 <=17 && edad2 >=13) {  
+    console.log('No podes entrar tu edad es menos de 18 tu edad es :'+edad2);
+ }else if (edad2 <=12) {
+    console.log('te falta mucchooo pa entrar apenas tienes: '+edad2);
+ }
+ //operadores logicos 
+ // != diferente
+ //== similar a algo
+ //=== estrictamete igual a resultado
+ //<= menor igual a algo
+ //>=mayor igual a algo
+ //< menor que algo
+ //> mayor que algo
+ // || operador logico or uno u otro
+// && que se cumplan las dos condicipones
+//!a operador not ()no
+
+if (edad2 == 23 || (edad2 >=45 && edad2 <=55 )) {   
+}
---
+++ src/Component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Component() {
+  return (
+    <div>
+      <input type="text" />
+        <button>Actualizar</button>
+        <p>Texto input</p>
+        <p>Texto Actualizado</p>
+    </div>
+  )
+}
